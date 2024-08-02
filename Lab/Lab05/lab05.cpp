6c67c692f8e41839ffe019065fef8aeb49cec341
Windowxd01 <133748311+Windowxd01@users.noreply.github.com>
2024-08-02 09:56:23 +0700

    Create

--- Lab/Lab05/lab05.cpp
+++ Lab/Lab05/lab05.cpp
@@ -1,36 +1,64 @@
+/*
+    ผู้ใช้กรอกค่า "คะแนนดิบ" เข้ามาในระบบเพื่อต้องการตรวจสอบเกรดในรายวิชา Programming ซึ่งมีเกณฑ์การให้คะแนนคือ F อยู่ในช่วงคะแนนน้อยกว่า 50 , D อยู่ในช่วงระหว่าง 50 ถึง 55 , D+ อยู่ในช่วงระหว่าง 55 ถึง 60 , C อยู่ในช่วงระหว่าง 60 ถึง 65 , C+ อยู่ในช่วงระหว่าง 65 ถึง 70 , B อยู่ในช่วงระหว่าง 70 ถึง 75 , B+ อยู่ในช่วงระหว่าง 75 ถึง 80 , A อยู่ในช่วงคะแนนมากกว่า 80 ขึ้นไป
+    (กำหนดให้ข้อนี้ใช้คำสั่ง if else ได้เท่านั้น)
+    
+    Test case:
+        enter score :
+            80
+    Output:
+        A !
+
+    Test case:
+        enter score :
+            55
+    Output:
+        D+ !
+
+    Test case:
+        enter score :
+            64
+    Output:
+        C !
+
+    Test case:
+        enter score :
+            44
+    Output:
+        F !
+
+    Test case:
+        enter score :
+            hello
+    Output:
+        please enter number only.
+*/
+
 #include <stdio.h>
-
-int main() {
-
-    int score;
-
-    printf("Enter Score : ");
-    scanf("%d", &score);
-    switch (score) {
-    case 1:
-        cout << "Monday";
-        break;
-    case 2:
-        cout << "Tuesday";
-        break;
-    case 3:
-        cout << "Wednesday";
-        break;
-    case 4:
-        cout << "Thursday";
-        break;
-    case 5:
-        cout << "Friday";
-        break;
-    case 6:
-        cout << "Saturday";
-        break;
-    case 7:
-        cout << "Sunday";
-        break;
+int main () {
+    int score = 0 ;
+    printf( "Please enter your score : ") ;
+    if( scanf ( "%d", &score ) !=0 ) {
+        printf( "Please Enter Number Only" );
+        return 1 ;
     }
-    // Outputs "Thursday" (day 4)
 
+    if( score >= 80 ) {
+        printf( "A" ) ;
+    } else if ( score >= 75 && score < 80 ) {
+        printf( "B+" ) ;
+    } else if ( score >= 70 && score < 75 ) {
+        printf( "B" ) ;
+    } else if ( score >= 65 && score < 70 ) {
+        printf( "C+" ) ;
+    } else if ( score >= 60 && score < 65 ) {
+        printf( "C" ) ; 
+    } else if ( score >= 55 && score < 60 ) {
+        printf( "D+" ) ;
+    } else if ( score >= 50 && score < 55 ) {
+        printf( "D" ) ;
+    } else {
+        printf( "F" );
+    } //end if
+    
     return 0 ;
-}//end main function
-
+}//end Function
